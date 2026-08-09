55a6191ec43f14a482e26cde6161ae8d7ae37450
Dabid <dabid4105@gmail.com>
2026-08-09 23:36:37 +0545

    Fix Modules.php: remove property types

--- app/Config/Modules.php
+++ app/Config/Modules.php
@@ -6,10 +6,10 @@ use CodeIgniter\Modules\Modules as BaseModules;
 
 class Modules extends BaseModules
 {
-    public bool $enabled = true;
-    public bool $discoverInComposer = true;
-    public array $composerPackages = [];
-    public array $aliases = [
+    public $enabled = true;
+    public $discoverInComposer = true;
+    public $composerPackages = [];
+    public $aliases = [
         'events',
         'filters',
         'registrars',
