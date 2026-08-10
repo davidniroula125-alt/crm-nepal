97d77746981c06413a13474f5ff9b7de937fd03a
Dabid <dabid4105@gmail.com>
2026-08-10 10:57:58 +0545

    Debug: test public BaseController vs admin BaseController

---
+++ app/Controllers/AdminTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class AdminTest extends BaseController
+{
+    public function index()
+    {
+        return "Public BaseController with Admin namespace test OK";
+    }
+}
