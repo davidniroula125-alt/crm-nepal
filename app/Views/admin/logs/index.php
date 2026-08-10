3db617afda3e470df8bb7b9b999839c3fe1a0046
Dabid <dabid4105@gmail.com>
2026-08-10 17:44:46 +0545

    fix: use getPageCount() instead of getTotalPages() in logs view

--- app/Views/admin/logs/index.php
+++ app/Views/admin/logs/index.php
@@ -35,7 +35,7 @@
     </table>
 </div>
 
-<?php if (isset($pager) && $pager->getTotalPages() > 1): ?>
+<?php if (isset($pager) && $pager->getPageCount() > 1): ?>
 <div class="admin-pagination">
     <?= $pager->links() ?>
 </div>
